cebfb265a9a647a24e49653d6f72d82d8b6cc410
Ivan2593 <i.drobyshevskii@g.nsu.ru>
2022-04-09 17:31:17 +0700

    commit

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,7 +4,12 @@
 - age - число
 - gender - 'male' или 'female'
  */
-export function getPersons();
+type people = {
+    name: string;
+    age: number;
+    gender: 'male' | 'female';
+};
+export function getPersons(): Array<people>;
 
 /*
 Типизируйте функцию personToString, возвращающую строку и принимающую объект одного из следующих форматов:
@@ -18,4 +23,7 @@ export function getPersons();
    - gender - 'male' или 'female'
    - company- строка
  */
-export function personToString(person);
+
+export function personToString(
+    obj: people | (people & { company: string }),
+): string;
